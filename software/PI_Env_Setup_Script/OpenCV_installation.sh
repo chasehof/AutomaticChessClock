67ae037215204063133079e86b7f87447ebd25df
conner hoffman <connermhoffman@gmail.com>
2025-12-03 23:33:37 -0500

    Initial Updates to BOM + Open CV QOL

---
+++ software/PI_Env_Setup_Script/OpenCV_installation.sh
@@ -0,0 +1,10 @@
+# Launch a Bash Shell 
+# Update the System
+sudo apt update
+sudo apt upgrade
+
+# Install Neccessary Dependencies for OpenCV
+sudo apt install build-essential cmake git libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev
+
+# Install OpenCV
+pip install opencv-python
